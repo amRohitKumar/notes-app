9f16c8a40cec52fd83be83fd8445653f47fb6a01
shutupRohit <shutupRohit@gmail.com>
2021-03-08 11:31:58 +0530

    Authentiation done

--- homePage.js
+++ homePage.js
@@ -1,14 +1,14 @@
 // Your web app's Firebase configuration
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAdOe_JssV-5HlVYM0GhFwKlT_G_d-8q2M",
-//     authDomain: "notes-app-a51b7.firebaseapp.com",
-//     projectId: "notes-app-a51b7",
-//     storageBucket: "notes-app-a51b7.appspot.com",
-//     messagingSenderId: "415019923323",
-//     appId: "1:415019923323:web:601a346c65aca6fee42fde"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+    apiKey: "AIzaSyAdOe_JssV-5HlVYM0GhFwKlT_G_d-8q2M",
+    authDomain: "notes-app-a51b7.firebaseapp.com",
+    projectId: "notes-app-a51b7",
+    storageBucket: "notes-app-a51b7.appspot.com",
+    messagingSenderId: "415019923323",
+    appId: "1:415019923323:web:601a346c65aca6fee42fde"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 
@@ -16,14 +16,34 @@ const addNote = document.querySelector('#addnote');
 const signOut = document.querySelector('#signout');
 const userName = document.querySelector('#username')
 
-// window.addEventListener('load', () => {
-//     const userid = localStorage.getItem('ID');
+window.addEventListener('load', () => {
+    const userid = localStorage.getItem('ID');
 
-//     var database = firebase.database().ref('User/'+userid);
+    var database = firebase.database().ref('User/' + userid);
 
-//     database.on('value', (snapshot) => {
-//         const username = snapshot.val().firstName+ ' ' + snapshot.val().lastName;
-//         // console.log(username);
-//         userName.innerText = 'Welcome ' + username + '  !';
-//       });
-// })
+    database.on('value', (snapshot) => {
+        const username = snapshot.val().firstName + ' ' + snapshot.val().lastName;
+        // console.log(username);
+        userName.innerText = 'Welcome ' + username + '  !';
+        userName.classList.add('capital');
+    });
+})
+
+addNote.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = "addNote.html";
+})
+
+signOut.addEventListener('click', () => {
+    firebase.auth().signOut()
+        .then(() => {
+            window.location.replace("loginPage.html"); 
+            window.alert('Logout Successfully !')
+
+        }).catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            window.alert(errorCode, errorMessage);
+        });
+})
--- signUp.js
+++ signUp.js
@@ -7,12 +7,13 @@ var firebaseConfig = {
     storageBucket: "notes-app-a51b7.appspot.com",
     messagingSenderId: "415019923323",
     appId: "1:415019923323:web:601a346c65aca6fee42fde"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 const button = document.querySelector('#contactForm');
+let flag = document.querySelector(".flag");
 
 button.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -21,29 +22,26 @@ button.addEventListener('submit', (e) => {
     let email = document.querySelector("#email").value;
     let password = document.querySelector("#password").value;
 
+
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then(() => {
-            window.alert('Congo !!! User registration sucessfull');
             let ID = firebase.auth().currentUser.uid;
-            firebase.database().ref('User/'+ ID).set({
+            firebase.database().ref('User/' + ID).set({
                 firstName: fname,
                 lastName: lname,
                 email: email
             })
+            window.alert('Congo !!! User registration sucessfull');
         })
         .then(() => {
-            let id = firebase.auth().currentUser.uid;
-            localStorage.setItem('ID', id);
+            flag.classList.remove('none');
         })
-        // .then(() => {
-        //     window.location.href = "file:///D:/Notes-App/homePage.html";
-        // })
         .catch((error) => {
             let errorcode = error.code;
             let errormsg = error.message;
 
-            window.alert('Something went wrong !',errorcode, errormsg);
+            window.alert('Something went wrong !', errorcode, errormsg);
         })
-    
+
     document.querySelector("#contactForm").reset();
 })
